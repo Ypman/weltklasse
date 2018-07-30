4ce6a044660865c75d41befcedf37d373b70f674
Ypman <41924625+Ypman@users.noreply.github.com>
2018-07-30 20:50:00 +0200

    Create weltklasse.py

---
+++ weltklasse.py
@@ -0,0 +1,33 @@
+class entity: 
+	pass
+
+class living_beeings(entity): 
+	pass
+	
+class machines(entity): 
+	pass
+
+class bird(living_beeings): 
+	pass
+	
+class fish(living_beeings): 
+	pass
+
+class vehicle(machines): 
+	pass
+
+class hawk(bird): 
+	pass
+	
+class shark(fish): 
+	pass
+	
+class bycicle(vehicle): 
+	pass
+	
+class car(vehicle): 
+	pass
+	
+class vw(car): 
+	pass 
+	
